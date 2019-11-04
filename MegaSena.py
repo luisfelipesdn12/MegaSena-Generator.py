58d635326f5b2793058918d27d9373ba92000700
gabriel-vernilo <53917092+gabriel-vernilo@users.noreply.github.com>
2019-11-03 21:46:26 -0300

    Add files via upload
    apenas reduzindo as definições de variável para uma única linha :)

--- MegaSena.py
+++ MegaSena.py
@@ -7,19 +7,8 @@ import random as r
 qtd = int(input("Quantos jogos você quer fazer ?\n"))
 feitos = 0
 while feitos < qtd :
-    a = r.randint(1, 60)
-    b = r.randint(1, 60)
-    c = r.randint(1, 60)
-    d = r.randint(1, 60)
-    e = r.randint(1, 60)
-    f = r.randint(1, 60)
-    while a == b or a==c or a==d or a==e or a==f or b==c or b==d or b==e or b==f or c==d or c==e or c==f or d==e or d==f or e==f: #nao repetir 
-        a = r.randint(1, 60)
-        b = r.randint(1, 60)
-        c = r.randint(1, 60)
-        d = r.randint(1, 60)
-        e = r.randint(1, 60)
-        f = r.randint(1, 60)
-
+    a,b,c,d,e,f = r.randint(1, 60),r.randint(1, 60),r.randint(1, 60),r.randint(1, 60),r.randint(1, 60),r.randint(1, 60)
+    while a == b or a==c or a==d or a==e or a==f or b==c or b==d or b==e or b==f or c==d or c==e or c==f or d==e or d==f or e==f: #nao repetir na mesma linha
+        a,b,c,d,e,f = r.randint(1, 60),r.randint(1, 60),r.randint(1, 60),r.randint(1, 60),r.randint(1, 60),r.randint(1, 60)
     print(a, b, c, d, e, f)
     feitos += 1
